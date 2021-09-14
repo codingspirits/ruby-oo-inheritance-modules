070863fbbb4db796f8964511b77231f251c04a40
vincci-cheng-project-development-page <vincci.cheng.github@gmail.com>
2021-09-14 22:57:06 +1000

    Completed Assignment

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,28 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Dancer 
+    include Dance
+
+    attr_accessor :name
+    def initialize(name)
+        @name = name 
+    end 
+
+end 
+
+class Dancers
+    extend MetaDancing
+end
+
+class Dancerz
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+  end
+  
+  angelina = Dancerz.new
+  angelina.twirl
+  // returns "I'm twirling!"
+  angelina.jump
+  // returns "Look how high I'm jumping!"
+  Dancerz.metadata
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,28 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+    include Dance
+    
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+end
+
+class Kids
+    extend MetaDancing
+end
+
+class Kidz
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+  end
+
+  buster = Kidz.new
+  buster.jump
+  // returns "Look how high I'm jumping!"
+  buster.take_a_bow
+  // returns "Thank you, thank you. It was a pleasure to dance for you all."
